85767d6c63b3aa0977989ecbbc4b47d14fc8f574
cubelet3d <cubelet@protonmail.com>
2021-02-14 14:57:21 -0800

    exec

--- inventory/js/inventory.js
+++ inventory/js/inventory.js
@@ -85,7 +85,7 @@ async function loadDistributor() {
             
             /*  Valentine's Day 2021
                 Available until Tue Feb 16 2021 21:00:00 GMT+0000
-            
+            */
             
             if(i = 34) {
                 let now = Math.round(new Date().getTime()/1000)
@@ -106,7 +106,7 @@ async function loadDistributor() {
                 }
             }
 			
-			*/
+			
             
             
             
