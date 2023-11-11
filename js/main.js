7261c002de10347fd59e139f38972db94523518c
cubelet3d <68728119+cubelet3d@users.noreply.github.com>
2023-11-11 12:43:26 +0200

    New wps
    I can't stop

--- js/main.js
+++ js/main.js
@@ -353,7 +353,7 @@ function showContent(tab) {
 }
 
 // Randomize wallpapers
-const numberOfWallpapers = 22;
+const numberOfWallpapers = 25;
 const wallpapersDirectory = "https://team3d.io/img/wallpapers/";
 function randomWallpapers() {
     let wp = localStorage.getItem("wallpaper")
