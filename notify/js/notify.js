b27682c6ecd8769986d85473f8324f4be186d1f1
cubelet3d <68728119+cubelet3d@users.noreply.github.com>
2023-11-23 12:58:05 +0200

    Revised notification system
    Captures the notify and error box ID's before incrementing the global variable. This should eliminate race conditions that made the notifications disappear too quickly sometimes.

--- notify/js/notify.js
+++ notify/js/notify.js
@@ -1,118 +1,112 @@
-let notifications = 0
-let errors = 0
+let notifications = 0; 
+let errors        = 0; 
 
-let notificationBox = '\
-<div class="notify">\
-    <div class="notify-icon-wrapper">\
-        <div class="notify-icon"></div>\
-    </div>\
-    <div class="notify-msg-wrapper">\
-        <div class="notify-msg"></div>\
-    </div>\
-    <div class="notify-btn-wrapper">\
-        <div class="notify-btn">OK</div>\
-    </div>\
-</div>'
+let notificationBox = 
+`<div class="notify">
+    <div class="notify-icon-wrapper">
+        <div class="notify-icon"></div>
+    </div>
+    <div class="notify-msg-wrapper">
+        <div class="notify-msg"></div>
+    </div>
+    <div class="notify-btn-wrapper">
+        <div class="notify-btn">OK</div>
+    </div>
+</div>`; 
 
-let errorBox = '\
-<div class="error-box">\
-  <div class="error-icon-wrapper">\
-    <div class="error-icon"></div>\
-  </div>\
-  <div class="error-msg-wrapper scrollbar">\
-    <div class="error-msg"></div>\
-  </div>\
-  <div class="error-btn-wrapper">\
-    <div class="error-btn inventory-button">OK</div>\
-  </div>\
-</div>'
+let errorBox = 
+`<div class="error-box">
+	<div class="error-icon-wrapper">
+		<div class="error-icon"></div>
+	</div>
+	<div class="error-msg-wrapper scrollbar">
+		<div class="error-msg"></div>
+	</div>
+	<div class="error-btn-wrapper">
+		<div class="error-btn inventory-button">OK</div>
+	</div>
+</div>`; 
 
 function notify(msg, action) {
-	audio.msage.play() 
-    // increment notifications counter 
-    notifications++
+	audio.msage.play(); 
+	
+	let thisNotificationId = notifications;
+    notifications++; 
     
-    // make a new box based on template and add notifications int as data attribute
-    let a = $(notificationBox).attr("data", notifications)
+    let a = $(notificationBox).attr("data", thisNotificationId);
+	
+	// find OK button 
+    let b = $(a).find("div.notify-btn"); 
+    $(b).attr("data", thisNotificationId); 
     
-    // find the OK button
-    let b = $(a).find("div.notify-btn")
-    
-    // add notifications int as data attribute on the button too (so we'll know which box it will close, duh)
-    $(b).attr("data", notifications)
-    
-    // find notify-msg 
-    let c = $(a).find("div.notify-msg")
-    
-    // append the msg
-    $(c).append(msg)
+	// find message div 
+    let c = $(a).find("div.notify-msg"); 
+    $(c).append(msg); 
     
     // Custom actions 
     switch(action) {
-        
         case "signMessage":
-            let d = $(a).find("div.notify-btn-wrapper")
-            $(d).html('<div class="sign-btn" data="'+notifications+'">OK</div>')
+            let d = $(a).find("div.notify-btn-wrapper"); 
+            $(d).html('<div class="sign-btn" data="'+thisNotificationId+'">OK</div>'); 
             break;
-            
         case "reload":
-            let u = $(a).find("div.notify-btn-wrapper")
-            $(u).html('<div class="reload-btn" data="'+notifications+'">OK</div>')
+            let u = $(a).find("div.notify-btn-wrapper"); 
+            $(u).html('<div class="reload-btn" data="'+thisNotificationId+'">OK</div>'); 
             break;
-            
-    }
+		}
     
     // append box to body
-    $("body").append(a)
+    $("body").append(a); 
     $(a).animate({
         "opacity":"1",
         "bottom":"64px"
-    })
+    }); 
 	
 	setTimeout(function() {
-		$('.notify[data='+notifications+']').animate({"opacity":"0","bottom":"-250px"})
-	}, 20000)
-
+		$('.notify[data='+thisNotificationId+']').animate({"opacity":"0", "bottom":"-250px"}); 
+	}, 5000);
 }
 
 function error(msg, warn) {
-	audio.error.play()
+	let thisErrorId = errors; 
 	errors++
-	let a = $(errorBox).attr("data", errors)
-	let b = $(a).find("div.error-btn")
-	$(b).attr("data", errors)
-	let c = $(a).find("div.error-msg")
+	let a = $(errorBox).attr("data", thisErrorId); 
+	let b = $(a).find("div.error-btn"); 
+	$(b).attr("data", thisErrorId); 
+	let c = $(a).find("div.error-msg"); 
 	if(warn) {
-		$(c).append('Warning:<br>'+msg)
+		// audio.warn.play(); 
+		$(c).append('Warning:<br>'+msg); 
 	} else {
-		$(c).append('Error:<br>'+msg)
+		audio.error.play();
+		$(c).append('Error:<br>'+msg); 
 	}
-    $("body").append(a)
+    $("body").append(a);
 }
 
 $(document).ready(function() {
     $("body").on("click", ".notify-btn", function() {
-        let id = $(this).attr("data")
-        $(".notify[data="+id+"]").hide()
-    })
+        let id = $(this).attr("data"); 
+        $(".notify[data="+id+"]").hide(); 
+    }); 
+	
     $("body").on("click", ".error-btn", function() {
-        let id = $(this).attr("data")
-        $(".error-box[data="+id+"]").hide()
-    })
+        let id = $(this).attr("data"); 
+        $(".error-box[data="+id+"]").hide(); 
+    }); 
     
     // sign.js special 
     $("body").on("click", ".sign-btn", function() {
-        let messageToSign = $("#messageToSign").val()
+        let messageToSign = $("#messageToSign").val(); 
         if(messageToSign !== "") {
-            signMessage(messageToSign)
+            signMessage(messageToSign); 
         }
-
-        let id = $(this).attr("data")
-        $(".notify[data="+id+"]").hide()
+        let id = $(this).attr("data"); 
+        $(".notify[data="+id+"]").hide(); 
     })
     
     // reload action
     $("body").on("click", ".reload-btn", function() {
-        location.reload()
+        location.reload(); 
     })
 })
