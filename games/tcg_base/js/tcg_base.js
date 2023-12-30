4a90410ccb4bb820c41b812dba5a8adf24b31d79
cubelet3d <68728119+cubelet3d@users.noreply.github.com>
2023-12-30 04:36:06 +0200

    Agnosia
    Process Deck in parallel making it near instant compared to the old method.

--- games/tcg_base/js/tcg_base.js
+++ games/tcg_base/js/tcg_base.js
@@ -2087,7 +2087,7 @@ async function tcg_base_fetchUserCards(player) {
 }
 
 // Returns tokenUris based on array of tokenIds 
-async function tcg_base_fetchTokenUris(tokenIds) {
+/*async function tcg_base_fetchTokenUris(tokenIds) {
 	try {
 		let tokenUris = []
 		for(let i = 0; i < tokenIds.length; i++) { 
@@ -2102,6 +2102,25 @@ async function tcg_base_fetchTokenUris(tokenIds) {
 	catch(e) {
 		console.error(e)
 	}
+}*/
+
+// Returns tokenUris based on array of tokenIds in parallel 
+async function tcg_base_fetchTokenUris(tokenIds) {
+    try {
+        // Create an array of promises for each token URI fetch
+        const uriPromises = tokenIds.map(tokenId => 
+            tcg_base_system.card.methods.tokenURI(tokenId).call().then(uri => {
+                let json = JSON.parse(atob(uri.slice(29)));
+                json.tokenId = tokenId;
+                return json;
+            })
+        );
+
+        // Wait for all promises to resolve
+        return await Promise.all(uriPromises);
+    } catch (e) {
+        console.error(e);
+    }
 }
 
 /*	This function is responsible for loading card data into the tokenIds list (on the right side)
